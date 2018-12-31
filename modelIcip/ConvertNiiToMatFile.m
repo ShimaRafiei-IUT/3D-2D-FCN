36dc36ef151aa2d61d0d122538388e0e8dec5939
ShimaRafiei-IUT <39675033+ShimaRafiei-IUT@users.noreply.github.com>
2018-12-31 18:37:07 +0330

    Update ConvertNiiToMatFile.m

--- modelIcip/ConvertNiiToMatFile.m
+++ modelIcip/ConvertNiiToMatFile.m
@@ -7,20 +7,20 @@ for n=index
     n
     if(n<10)
         Y22 = nii_read_volume([imageAddress '/img/img000' num2str(n) '.nii']);
-        %Y22 = imrotate(Y22,90);        
+        Y22 = imrotate(Y22,90);        
         save([imageAddress '/img/img000' num2str(n) '.mat'],'Y22');
         Y2 = nii_read_volume([imageAddress '/label/label000' num2str(n) '.nii']);
-        %Y2 = imrotate(Y2,90);        
+        Y2 = imrotate(Y2,90);        
 		figure,imshow3D(Y2);
         save([imageAddress '/label/label000' num2str(n) '.mat'],'Y2');
         
     else
         Y22 = nii_read_volume([imageAddress '/img/img00' num2str(n) '.nii']);
-        %Y22 = imrotate(Y22,90);        
+        Y22 = imrotate(Y22,90);        
         
         save([imageAddress '/img/img00' num2str(n) '.mat'],'Y22');        
         Y2 = nii_read_volume([imageAddress '/label/label00' num2str(n) '.nii']);
-        %Y2 = imrotate(Y2,90);        
+        Y2 = imrotate(Y2,90);        
         
 		figure,imshow3D(Y22);
         save([imageAddress '/label/label00' num2str(n) '.mat'],'Y2');
