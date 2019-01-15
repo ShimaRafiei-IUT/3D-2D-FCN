a0bd4da295e0f8439fc815ff86fe6b7e0bc78c55
ShimaRafiei-IUT <39675033+ShimaRafiei-IUT@users.noreply.github.com>
2019-01-15 22:00:19 +0330

    Delete GenerateUselessMask.m

--- modelIcip/GenerateUselessMask.m
+++
@@ -1,9 +0,0 @@
-function [boxx]=GenerateUselessMask(boxx)
-    boxx(boxx>-450)=1;
-    boxx(boxx<=-450)=0;
-    for s=1:size(boxx,3)
-        slice=boxx(:,:,s);
-        slice = imfill(slice, 'holes') ;
-        boxx(:,:,s)=GenerateMask(slice,0.5);
-    end    
-end
