701bb72d2bc104bf6b36036d07849a8aad98d9ee
ShimaRafiei-IUT <39675033+ShimaRafiei-IUT@users.noreply.github.com>
2019-01-15 22:01:26 +0330

    Delete BalancingWeightMat3D.m

--- modelIcip/BalancingWeightMat3D.m
+++
@@ -1,5 +0,0 @@
-function [BalancedWeight]=BalancingWeightMat3D(weight,rawData)
-    uselessMask=GenerateUselessMask(rawData);   
-    BalancedWeight=weight.*uselessMask;
-    BalancedWeight(uselessMask==0)=0.01;
-end
